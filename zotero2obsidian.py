529aa5bcbdfc58d3f7463d28ac854b75fdd01039
Septimus Hodge <matthewgroh@gmail.com>
2021-01-15 11:46:47 -0500

    Commenting automatic markdown creation because I decided all notes should be created manually

--- zotero2obsidian.py
+++ zotero2obsidian.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     # markdown_folder = "/Users/heart/mindspace/zotero research/"
     zotero_filepath = sys.argv[1]
     obsidian_filepath = sys.argv[2]
-    library_filepath = sys.argv[3]
-    markdown_folder = sys.argv[4]
+    # library_filepath = sys.argv[3]
+    # markdown_folder = sys.argv[4]
 
     # Step 1 - Collect all .pdfs in Zotero directory and copy to Obsidian directory
     filepath_map = collect_pdfs(zotero_filepath, obsidian_filepath)
@@ -34,22 +34,22 @@ if __name__ == "__main__":
 
     # Step 2  - Create markdown files in Obsidian with metadata from all Zotero pdfs
 
-    df = pd.read_csv(library_filepath)
-    df = df[["Key",'Author',"Date","Title","Short Title","Url",'File Attachments','Automatic Tags']]
-    df.columns = ["key","authors","date","title","publication_title","url","zotero_filepath","tags"]
-    df["obsidian_filepath"] = df.zotero_filepath.str.replace(zotero_filepath, '', regex=True)
-    df["obsidian_filepath"] = df.obsidian_filepath.str.replace(".*/", '', regex=True)
-    df["obsidian_filepath"] = df.obsidian_filepath.str.replace(" ", '_', regex=True)
-    print(os.listdir(markdown_folder))
-    for index, row in df.iterrows():
-        row["title"] = re.sub("/","",row["title"])
-        if "{}.md".format(row["title"]) not in os.listdir(markdown_folder):
-            f = open("{}{}.md".format(markdown_folder, row["title"]), "w")
-            f.write("""
-    Title: [{}](pdfs/zotero/{})\n
-    Authors: {}\n
-    Notes:
-            """.format(row["title"],row["obsidian_filepath"],row["authors"],))
-            f.close()
+    # df = pd.read_csv(library_filepath)
+    # df = df[["Key",'Author',"Date","Title","Short Title","Url",'File Attachments','Automatic Tags']]
+    # df.columns = ["key","authors","date","title","publication_title","url","zotero_filepath","tags"]
+    # df["obsidian_filepath"] = df.zotero_filepath.str.replace(zotero_filepath, '', regex=True)
+    # df["obsidian_filepath"] = df.obsidian_filepath.str.replace(".*/", '', regex=True)
+    # df["obsidian_filepath"] = df.obsidian_filepath.str.replace(" ", '_', regex=True)
+    # print(os.listdir(markdown_folder))
+    # for index, row in df.iterrows():
+    #     row["title"] = re.sub("/","",row["title"])
+    #     if "{}.md".format(row["title"]) not in os.listdir(markdown_folder):
+    #         f = open("{}{}.md".format(markdown_folder, row["title"]), "w")
+    #         f.write("""
+    # Title: [{}](pdfs/zotero/{})\n
+    # Authors: {}\n
+    # Notes:
+    #         """.format(row["title"],row["obsidian_filepath"],row["authors"],))
+    #         f.close()
     print("done")
 
